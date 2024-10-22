b1696bc80542bebc38b0384ac849e80207327026
Vincent Bourgeois <vincent_bourgeois@natural-solutions.eu>
2024-10-22 20:00:21 +0200

    feat: wip multi langs

--- backend/routes.py
+++ backend/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, Blueprint, abort
+from flask import render_template, Blueprint, abort, request, redirect, url_for
+from functools import wraps
 from sqlalchemy import text
 from sqlalchemy.sql.expression import desc
 import models
@@ -21,12 +22,31 @@ site_schema = models.TSiteSchema(many=True)
 themes_sthemes_schema = models.CorSthemeThemeSchema(many=True)
 communes_schema = models.CommunesSchema(many=True)
 
+def localeGuard(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        locale = utils.getLocale()
+        if not utils.isMultiLangs() and locale is not None:
+            return redirect(url_for(request.endpoint))
+        if utils.isMultiLangs() and locale is None:
+            matched_locale = request.accept_languages.best_match(['fr', 'en'])
+            if matched_locale is None:
+                return redirect('/')
+            return redirect(url_for(request.endpoint, locale=matched_locale))
+        return f(*args, **kwargs)
+    return decorated_function
 
-@main.route("/")
-def home():
-    sql = text(
-        "SELECT * FROM geopaysages.t_site p join geopaysages.t_observatory o on o.id=p.id_observatory where p.publish_site=true and o.is_published is true ORDER BY RANDOM() LIMIT 6"
-    )
+def homeLocaleSelector():
+    return "Select a language"
+
+@main.route('/')
+@main.route('/<string:locale>/')
+def home(locale=None):
+    if utils.isMultiLangs() and locale is None:
+        return homeLocaleSelector()
+    if not utils.isMultiLangs() and locale is not None:
+        return redirect('/')
+    sql = text("SELECT * FROM geopaysages.t_site p join geopaysages.t_observatory o on o.id=p.id_observatory where p.publish_site=true and o.is_published is true ORDER BY RANDOM() LIMIT 6")
     sites_proxy = db.engine.execute(sql).fetchall()
     sites = [dict(row.items()) for row in sites_proxy]
 
@@ -220,9 +240,12 @@ def site_photos_last(id_site):
     return render_template("site_photo.jinja", site=site, photo=photo)
 
 
-@main.route("/sites")
-def sites():
+@main.route('/sites')
+@main.route('/<string:locale>/sites/')
+@localeGuard
+def sites(locale=None):
     data = utils.getFiltersData()
+    print(locale)
 
     return render_template(
         "sites.jinja",
@@ -232,7 +255,9 @@ def sites():
     )
 
 
-@main.route("/legal-notices")
+@main.route('/legal-notices/')
+@main.route('/<string:locale>/legal-notices/')
+@localeGuard
 def legal_notices():
 
     tpl = utils.getCustomTpl("legal_notices")
